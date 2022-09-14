9d23478dc11a15f2d5c5116f405d36f11d53804d
ThatRandomGamer1337 <113189409+ThatRandomGamer1337@users.noreply.github.com>
2022-09-14 13:55:12 -0300

    First Commit

--- HelloWorldFinale/Program.cs
+++ HelloWorldFinale/Program.cs
@@ -5,26 +5,26 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace HelloWorldFinale
-internal class Program
 {
-    static void Main(string[] args)
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Please put in your name: ");
+                string name = Console.ReadLine();
 
-        Console.WriteLine("Please put in your name: ");
-        string name = Console.ReadLine();
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine("- - - - - - - - - - - - - - - -");
+                Console.WriteLine($"Hello '{name}', have a good day!");
+                Console.WriteLine("- - - - - - - - - - - - - - - -");
+                Console.Beep();
+                Console.WriteLine(" ");
+                Console.WriteLine(" ");
+                Console.WriteLine("Program by Serena Burnett");
+                Console.WriteLine(" ");
+            Console.ReadKey(true);
 
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        Console.WriteLine("- - - - - - - - - - - - - - - -");
-        Console.WriteLine($"Hello '{name}', have a good day!");
-        Console.WriteLine("- - - - - - - - - - - - - - - -");
-        Console.Beep();
-        Console.WriteLine(" ");
-        Console.WriteLine(" ");
-        Console.WriteLine("Program by Serena Burnett");
-        Console.WriteLine(" ");
-        Console.ReadKey(true);
-
+            }
+        }
     }
-}
-}
